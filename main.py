e90290d981f483228ac30fdea146dcd46e633091
Lee Hyunuk <as0266@naver.com>
2023-04-10 00:26:04 +0900

    다운로드와 상태확인 api 분리

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, UploadFile, BackgroundTasks
+from fastapi import FastAPI, Form, UploadFile, BackgroundTasks, HTTPException
 from fastapi.responses import RedirectResponse, FileResponse
 from img_process import crop_and_save
 from selenium_clova import file_ocr
@@ -50,7 +50,32 @@ def crop_and_process(
         "message" : "success"
     }
 
-@app.get("/process/file")
+@app.get("/process/status")
+def get_file(
+    name: str = "", 
+    year: str = "",
+    month: str = ""
+):
+    file_name = '{} {}년 {}월 법인카드 내역.xlsx'.format(name, year, month)
+    file_path = "result/" + file_name
+    if os.path.exists(file_path):
+        return {
+            "state": "complete"
+        }
+    else:
+        img_exists = os.path.isdir("screenshots/{}_{}_{}".format(name, year, month))
+        with open("process.txt", "r", encoding="utf8") as f:
+            state = f.read()
+        if img_exists and state=="_".join([name, year, month]):
+            return {
+                "state": "processing"
+            }
+        else:
+            return {
+                "state": "not found"
+            }
+        
+@app.get("/download")
 def get_file(
     name: str = "", 
     year: str = "",
@@ -65,17 +90,7 @@ def get_file(
             media_type= "application/`vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
     else:
-        img_exists = os.path.isdir("_".join([name, year, month]))
-        with open("process.txt", "r", encoding="utf8") as f:
-            state = f.read()
-        if img_exists and state=="_".join([name, year, month]):
-            return {
-                "state": "processing"
-            }
-        else:
-            return {
-                "state": "not found"
-            }
+        raise HTTPException(400)
 
 @app.get("/process/now")
 def get_now():
